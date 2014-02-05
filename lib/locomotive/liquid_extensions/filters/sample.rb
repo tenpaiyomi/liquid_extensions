b12846a3c0758bced35e00adf3912e0a6ff1f5ed
Eric Musgrove <musgrove.eric@gmail.com>
2014-02-05 17:39:22 -0500

    Going about this another way

--- lib/locomotive/liquid_extensions/filters/sample.rb
+++ lib/locomotive/liquid_extensions/filters/sample.rb
@@ -3,12 +3,14 @@ module Locomotive
     module Filters
       module Sample
 
-        def sample(input, number = 1)
+        def sample(input, number = 1, response = nil)
           if input.respond_to?(:all) # Content type collection
-            number > 1 ? input.all.sample(number) : input.all.sample
+            response = number > 1 ? input.all.sample(number) : input.all.sample
           else
-            number > 1 ? input.sample(number) : input.sample
+            response = number > 1 ? input.sample(number) : input.sample
           end
+
+          return response.body
         end
 
       end
